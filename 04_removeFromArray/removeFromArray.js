9d3309c9decc01b77249e2c521bd22ea8f15ab22
AaronDuong <aarond78@gmail.com>
2022-03-02 13:18:46 -0500

    Completed exercise 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr) {
+// 	const [, ...targetItems] = arguments;
+// 	let newArr = [...arr];
 
+// 	for (let target of targetItems) {
+// 		newArr = newArr.filter((item) => item !== target);
+// 	}
+// 	return newArr;
+// };
+
+const removeFromArray = function (...args) {
+	const array = args[0];
+	// console.log(array);
+	// console.log(args);
+	return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
